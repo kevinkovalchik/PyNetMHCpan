63556b1ab69588eb863c83494b137402662ae048
kevin <kkovalchik.chusj@gmail.com>
2020-12-11 09:35:11 -0500

    fix missing config file

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 setup(
     name='PyNetMHCpan',
-    version='0.1.1',
+    version='0.1.2',
     packages=['PyNetMHCpan'],
     url='https://github.com/kevinkovalchik/PyNetMHCpan',
     license='MIT',
     author='Kevin Kovalchik',
+    include_package_data=True,
     author_email='',
     description='A simple tool for using NetMHCpan and NetMHCIIpan using multiple CPUs in a Python environment.',
     install_requires=["numpy", "pandas"]
