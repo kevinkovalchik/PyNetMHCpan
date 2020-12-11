af573b595121b4fe3f764586d8d52adc9b7b7b11
kevin <kkovalchik.chusj@gmail.com>
2020-12-11 08:33:37 -0500

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='PyNetMHCpan',
-    version='0.1.0',
+    version='0.1.1',
     packages=['PyNetMHCpan'],
     url='https://github.com/kevinkovalchik/PyNetMHCpan',
     license='MIT',
